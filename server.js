ead442862212086ec9ed44120ba9c176e92eddcb
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-20 18:57:45 +0300

    added database information

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var deployd = require('deployd');
 
-var dpd = deployd();
+var dpd = deployd(
+    db: {
+        host: 'ds031832.mongolab.com',
+        port: 31832,
+        name: 'jobserver',
+        credentials: {
+            username: 'hamidabdulmalik',
+            password: 'mi566666'
+        }
+    });
 
 dpd.listen();
