2ebb33ce9ffd5acadd679b7bff6b6ac6f1291228
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-20 21:15:36 +0300

    changed server.js to index.js and added more configuration

---
+++ package.js
@@ -0,0 +1,24 @@
+{
+    "name": "jobserver",
+    "version": "0.0.1",
+    "description": "A sample deployd app,
+    "main": "index.js",
+    "scripts": {
+        "start": "node index.js"
+    },
+    "dependencies": {
+        "deployd": "0.8.x"
+    },
+    "engines": {
+        "node": "0.12.2"
+    },
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/zimakki/JobServer"
+    },
+    "keywords": [
+        "node",
+        "heroku",
+    ],
+    "license": "MIT"
+}
