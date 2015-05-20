06197b2ed998e37125dab2810b8e2658b79a43b2
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-20 19:08:58 +0300

    db field not required

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var deployd = require('deployd');
 
 var dpd = deployd({
-    db: {
-        host: 'ds031832.mongolab.com',
-        port: 31832,
-        name: 'jobserver',
-        credentials: {
-            username: 'hamidabdulmalik',
-            password: 'mi566666'
-        }
+    host: 'ds031832.mongolab.com',
+    port: 31832,
+    name: 'jobserver',
+    credentials: {
+        username: 'hamidabdulmalik',
+        password: 'mi566666'
     }
 });
-
 dpd.listen();
