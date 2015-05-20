410ebf4b60497f467ae47e903da01bf2159ebe27
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-20 20:47:19 +0300

    Rerversed all changes and removed package.json

--- server.js
+++ server.js
@@ -1,7 +1,4 @@
 var deployd = require('deployd');
 
-var dpd = deployd({
-    host: 'mongodb://hamidabdulmalik:mi566666@ds031832.mongolab.com:31832/jobserver',
-    port: 31832
-});
+var dpd = deployd();
 dpd.listen();
