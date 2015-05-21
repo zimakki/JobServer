891c5db2e6bb0baf4a785bc23459366a1e7565d1
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-21 03:07:08 +0300

    testing login success

--- public/factories/loginFactory.js
+++ public/factories/loginFactory.js
@@ -1,8 +1,13 @@
 jobs.factory('loginFactory', function ($rootScope, $location) {
     return {
         login: function (scope) {
-            dpd.users.login(scope.user, function (result, error) {
 
+            dpd.users.login(scope.user, function (result, error) {
+                if (error) {
+                    alert(JSON.stringify(error))
+                } else {
+                    alert(JSON.stringify(result))
+                }
             })
         }
     }
