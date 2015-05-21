dbea935a1044b8e49481ad6ee07a788a7d4bfb53
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-21 19:52:49 +0300

    pushed new company into existing list of companies

--- factories/companiesFactory.js
+++ factories/companiesFactory.js
@@ -1,7 +1,9 @@
 jobs.factory('companiesFactory', function ($rootScope, $location, $q) {
     return {
         saveCompany: function (scope) {
-            dpd.companies.post(scope.company, function (result, error) {})
+            dpd.companies.post(scope.company, function (result, error) {
+                scope.companies.push(result)
+            })
         }
     }
 })
