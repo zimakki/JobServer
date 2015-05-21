837e38101cb4c518222a5c9cda7ee34e4560bf9f
hamidabdulmalik@gmail.com <hamidabdulmalik@gmail.com>
2015-05-21 05:16:00 +0300

    Overall login/logut test successful

--- public/factories/loginFactory.js
+++ public/factories/loginFactory.js
@@ -4,9 +4,9 @@ jobs.factory('loginFactory', function ($rootScope, $location, signupFactory) {
 
             dpd.users.login(scope.user, function (result, error) {
                 if (error) {
-                    alert(JSON.stringify(error))
+                    //alert(JSON.stringify(error))
                 } else {
-                    alert(JSON.stringify(result))
+                    //alert(JSON.stringify(result))
                 }
                 signupFactory.watchSession()
 
