e18baf7e531bd832ec23bfd1b1e95d58267c2105
hamid abdulmalik <hamidabdulmalik@gmail.com>
2015-05-22 05:52:09 +0000

    added promise to capture changes

--- factories/companiesFactory.js
+++ factories/companiesFactory.js
@@ -1,9 +1,16 @@
 jobs.factory('companiesFactory', function ($rootScope, $location, $q) {
     return {
         saveCompany: function (scope) {
+            var defer = $q.defer()
+            
             dpd.companies.post(scope.company, function (result, error) {
-                scope.companies.push(result)
-                scope.company = {}
+                
+                defer.promise.then(function(result){
+                    scope.companies.push(result)
+                    scope.company.name = ''
+                    scope.company.description=''
+                })
+                defer.resolve(result)
             })
         },
 
