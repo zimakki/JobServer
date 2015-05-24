f21e75b9a3704b4a706908b03ce89f3413cd4179
hamid abdulmalik <hamidabdulmalik@gmail.com>
2015-05-24 21:46:48 +0000

    changed list of companies to a global object so that we dont have to hit the server all the time to get list of companies

--- controllers/admin_companies.js
+++ controllers/admin_companies.js
@@ -1,12 +1,12 @@
-jobs.controller('admin_companies',function($scope,$location,companiesFactory){
+jobs.controller('admin_companies',function($scope,$location,$rootScope,companiesFactory){
     $scope.company={
         name:'',
         description:''
     };
     
-    $scope.companies = []
-    
-    companiesFactory.getCompanies($scope)
+    if (!$rootScope.companies){
+        companiesFactory.getCompanies($scope)   
+    }
     
     $scope.save = function(){
         companiesFactory.saveCompany($scope)
--- factories/companiesFactory.js
+++ factories/companiesFactory.js
@@ -1,29 +1,31 @@
 jobs.factory('companiesFactory', function ($rootScope, $location, $q) {
     return {
         saveCompany: function (scope) {
-            var defer = $q.defer()
+            var defer = $q.defer();
             
             dpd.companies.post(scope.company, function (result, error) {
                 
                 defer.promise.then(function(result){
-                    scope.companies.push(result)
-                    scope.company.name = ''
-                    scope.company.description=''
-                })
-                defer.resolve(result)
-            })
+                    $rootScope.companies.push(result);
+                    scope.company.name = '';
+                    scope.company.description='';
+                });
+                defer.resolve(result);
+            });
         },
 
         getCompanies: function (scope) {
-            var defer = $q.defer()
+            var defer = $q.defer();
+            $rootScope.companies = [];
             dpd.companies.get({}, function (result, error) {
                 
                 defer.promise.then(function(result){
-                    scope.companies = result;
-                })
-                defer.resolve(result)
+                    //alert(JSON.stringify(result))
+                    $rootScope.companies = result;
+                });
+                defer.resolve(result);
                 
-            })
+            });
         }
-    }
-})
+    };
+});
