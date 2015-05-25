17c46505dfc20f0df889bbb8104c8fddb37a0dd2
hamid abdulmalik <hamidabdulmalik@gmail.com>
2015-05-25 00:07:03 +0000

    initialized password retype variable

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,6 +5,8 @@ jobs.controller('signup', function ($scope, signupFactory) {
         fullname: '',
         privilege: 'User'
     }
+    
+    $scope.passwordRetype = ''
 
     $scope.save = function () {
         signupFactory.signup($scope)
